801424c0d6a1db3fe97c6f761a03a5ef0962d474
Vic van Gool <vic@cloud66.com>
2019-12-02 11:33:38 +0000

    include branched exception tests

--- test/test_exception_extensions.rb
+++ test/test_exception_extensions.rb
@@ -34,6 +34,52 @@ module ExceptionExtensions
       assert_equal path[2], exception.cause.cause
     end
 
+    def test_branched_exception_path
+      threads = []
+      exceptions = []
+      exceptions_mutex = Mutex.new
+      threads << Thread.new do
+        capture_exception_in_array(exceptions, exceptions_mutex) { reraise_with(C) { reraise_with(B) { raise A } } }
+      end
+      threads << Thread.new do
+        capture_exception_in_array(exceptions, exceptions_mutex) { reraise_with(C) { reraise_with(F) { reraise_with(G) { raise H } } } }
+      end
+      threads << Thread.new do
+        capture_exception_in_array(exceptions, exceptions_mutex) { raise H }
+      end
+      threads << Thread.new do
+        capture_exception_in_array(exceptions, exceptions_mutex) do
+          internal_threads = []
+          internal_exceptions = []
+          internal_exceptions_mutex = Mutex.new
+          internal_threads << Thread.new do
+            capture_exception_in_array(internal_exceptions, internal_exceptions_mutex) { reraise_with(C) { reraise_with(B) { raise A } } }
+          end
+          internal_threads << Thread.new do
+            capture_exception_in_array(internal_exceptions, internal_exceptions_mutex) { raise H }
+          end
+          internal_threads.map(&:join)
+          raise I.new(internal_exceptions)
+        end
+      end
+      threads.map(&:join)
+      exception = capture_exception do
+        reraise_with(D) do
+          raise E.new(exceptions)
+        end
+      end
+      exception_traverser = ExceptionPathTraverser.new(exception)
+      path_array = exception_traverser.to_a
+      expected_path_classes = [[D, E, C, B, A], [D, E, C, F, G, H], [D, E, H], [D, E, I, C, B, A], [D, E, I, H]]
+      expected_number_of_paths = expected_path_classes.size
+      actual_path_classes = path_array.map { |path| path.map(&:class) }
+      actual_number_of_paths = actual_path_classes.size
+      assert_equal expected_number_of_paths, actual_number_of_paths
+      expected_path_classes.each do |path_class_array|
+        assert actual_path_classes.include?(path_class_array)
+      end
+    end
+
     private
 
     class A < StandardError;
@@ -53,6 +99,12 @@ module ExceptionExtensions
     class H < StandardError;
     end
     class I < StandardErrorCollection;
+    end
+
+    def capture_exception_in_array(array, mutex)
+      yield
+    rescue => exc
+      mutex.synchronize { array << exc }
     end
 
     def capture_exception
